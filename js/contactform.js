aaa40fc3a1532a755be73e772be12711ddd2034c
Kalvin Abraham-Mendez <kalvinjay@Kalvins-MacBook-Pro.local>
2022-09-28 22:47:52 -0500

    added js to page 3.1

--- js/contactform.js
+++ js/contactform.js
@@ -6,16 +6,23 @@
         var selectPhone = document.getElementById("optionPhone");
         var inputEmail = document.getElementById("email");
         var inputPhone = document.getElementById("phone");
+        var labelemail = document.getElementById("emaillabel");
+        var labelphone = document.getElementById("phonelabel");
 
         if (selectEmail.selected) {
             inputEmail.style.display = "block";
             inputPhone.style.display = "none";
+            labelphone.style.display = "none";
+            labelemail.style.display = "block";
         } else if (selectPhone.selected) {
             inputEmail.style.display = "none";
             inputPhone.style.display = "block";
+            labelemail.style.display = "none";
+            labelphone.style.display = "block";
         } else {
             inputEmail.style.display = "none";
             inputPhone.style.display = "none";
+            labelemail.style.display = "none";
         }
     }
     function load() {
@@ -25,6 +32,10 @@
         var inputPhone = document.getElementById("phone");
         inputEmail.style.display = "none";
         inputPhone.style.display = "none";
+        var labelemail = document.getElementById("emaillabel");
+        var labelphone = document.getElementById("phonelabel");
+        labelemail.style.display = "none";
+        labelphone.style.display = "none";
     }
     
     document.addEventListener("DOMContentLoaded", load);
